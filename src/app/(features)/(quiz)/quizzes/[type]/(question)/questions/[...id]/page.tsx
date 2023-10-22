3c53eb4fd95937db1bb8685a6ef5a25438a6f3f5
dak2 <dawt2h@gmail.com>
2023-10-22 18:32:08 +0900

    feat: fetch current question
    最初の質問を取得する処理を実装

--- src/app/(features)/(quiz)/quizzes/[type]/(question)/questions/[...id]/page.tsx
+++ src/app/(features)/(quiz)/quizzes/[type]/(question)/questions/[...id]/page.tsx
@@ -1,8 +1,41 @@
+'use client';
+import { QuestionContext } from 'app/providers/QuestionContext';
+import { Suspense, use, useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { UserContext } from 'app/providers/UserProvider';
+import { getCurrentUser } from 'app/utils/auth';
+import { css } from '../../../../../../../../../styled-system/css';
+import { Question } from 'app/entity/Question';
+
+const loadingTextCss = css({
+  textAlign: 'center',
+  marginTop: '15%',
+  fontSize: '2rem',
+});
+
 // entry point of questions
-export default async function Page({ params }: { params: { type: string; id: number } }) {
+export default function Page({ params }: { params: { type: string; id: number } }) {
+  const { user } = useContext(UserContext);
+  const { questions } = useContext(QuestionContext);
+  const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>(undefined);
+  const router = useRouter();
+  const currentUser = getCurrentUser(user);
+  const handleUserRedirect = () => {
+    if (!currentUser.name) router.push('/');
+  };
+
+  useEffect(() => {
+    handleUserRedirect();
+    const questionId = Number(params.id);
+    const initCurrentQuestion = questions.find((q) => q.id === questionId);
+    setCurrentQuestion(initCurrentQuestion);
+  }, [questions, params.id]);
+
   return (
-    <div>
-      aaaaa: {params.id} / {params.type}
-    </div>
+    // TODO: 次の質問への遷移
+    <Suspense fallback={<p className={loadingTextCss}>Loading...</p>}>
+      <div>aaaaa: {questions.length}</div>
+      <div>aaaaa: {currentQuestion?.contents}</div>
+    </Suspense>
   );
 }
